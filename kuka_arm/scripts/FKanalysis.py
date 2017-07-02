8cb85cf123c5df021966b913cff1bd5d0080a20c
Till Barmeier <tbarmeier@googlemail.com>
2017-07-02 15:12:28 +0200

    minor changes in FKanalysis.py

--- kuka_arm/scripts/FKanalysis.py
+++ kuka_arm/scripts/FKanalysis.py
@@ -21,7 +21,7 @@ alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 s = {alpha0: 0,     a0:     0,  d1: 0.75, 
      alpha1: -pi/2, a1:  0.35,  d2: 0,       q2: q2-pi/2,
      alpha2: 0,     a2: 1.25,   d3: 0,
-     alpha3: -pi/2, a3:  -0.054, d4: 1.5,  # might have to add a sign for a3
+     alpha3: -pi/2, a3:  -0.054, d4: 1.5,  
      alpha4: pi/2,  a4: 0,      d5: 0,
      alpha5: -pi/2, a5: 0,      d6: 0,
      alpha6: 0,     a6: 0,      d7: 0.303, q7: 0}
